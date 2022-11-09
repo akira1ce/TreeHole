352b9a3790364c36e56b1b4965fbfb1a5e3516f1
amos <amoslier@163.com>
2022-11-09 10:59:15 +0800

    [add] treeSchema

---
+++ workspace/TreeHole-api/model/Tree.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const Types = require("../util/schemaTypes");
+const treeSchema = new mongoose.Schema({
+  ownerID: {
+    type: Types.String,
+    required: true,
+  },
+  type: {
+    type: Types.String,
+    required: true,
+  },
+  height: {
+    type: Types.String,
+    required: false,
+  },
+  diameter: {
+    type: Types.String,
+    required: false,
+  },
+  branchPoint: {
+    type: Types.String,
+    required: false,
+  },
+  location: {
+    type: Types.String,
+    required: false,
+    default: "",
+  },
+  describe: {
+    type: Types.String,
+    required: false,
+    default: "",
+  },
+  price: {
+    type: Types.String,
+    required: false,
+    default: "",
+  },
+  state: {
+    type: Types.String,
+    required: false,
+    default: "",
+  },
+  imgs: {
+    type: Types.Array,
+    required: false,
+    default: "",
+  },
+});
+module.exports = userSchema;
