8c3590145cc5f2d13d4f41f2b2214a88b6302e77
amos <amoslier@163.com>
2022-11-15 15:04:55 +0800

    [modify] jwt UnauthorizedError -> code 10000

--- workspace/TreeHole-api/middleware/error-handler.js
+++ workspace/TreeHole-api/middleware/error-handler.js
@@ -3,6 +3,10 @@ const { result } = require("../util");
 
 module.exports = () => {
   return (err, req, res, next) => {
+    if (err.name == "UnauthorizedError") {
+      res.send(result(10000, "", err.message || err));
+      return;
+    }
     res.send(result(err.status || 500, "", err.message || err));
   };
 };
