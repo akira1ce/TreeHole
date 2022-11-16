80256dc92b7d0a87ae73a705ffb07f9cbb274950
amos <amoslier@163.com>
2022-11-16 19:14:56 +0800

    [add] view components route

--- workspace/TreeHole/src/router/route.js
+++ workspace/TreeHole/src/router/route.js
@@ -1,6 +1,10 @@
 const Login = () => import("../view/Login.vue");
-const Home = () => import("../view/Home.vue");
 const Register = () => import("../view/Register.vue");
+const Home = () => import("../view/Home.vue");
+const Dynamic = () => import("../view/Dynamic.vue");
+const Personal = () => import("../view/Personal.vue");
+const Space = () => import("../view/Space.vue");
+const Socket = () => import("../view/Socket.vue");
 
 const routes = [
   {
@@ -17,6 +21,26 @@ const routes = [
     path: "/home",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/dynamic",
+    name: "Dynamic",
+    component: Dynamic,
+  },
+  {
+    path: "/personal",
+    name: "Personal",
+    component: Personal,
+  },
+  {
+    path: "/space",
+    name: "Space",
+    component: Space,
+  },
+  {
+    path: "/socket",
+    name: "Socket",
+    component: Socket,
   },
   { path: "/", redirect: "/login" },
 ];
