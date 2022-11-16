a0e2118418d5a510430eb61149002a7d2e2d20ac
amos <amoslier@163.com>
2022-11-16 19:13:28 +0800

    [add] sidebar、topbar

--- workspace/TreeHole/src/App.vue
+++ workspace/TreeHole/src/App.vue
@@ -1,9 +1,17 @@
-<script setup></script>
+<script setup>
+import Sidebar from "./components/Sidebar.vue";
+import Topbar from "./components/Topbar.vue";
+import { useRoute } from "vue-router";
+import { computed, reactive, ref, watch } from "vue-demi";
+
+const route = useRoute();
+const whitelist = ['Login','Register']
+</script>
 
 <template>
+  <Sidebar v-show="whitelist.indexOf(route.name)==-1" />
+  <Topbar v-show="whitelist.indexOf(route.name)==-1" />
   <router-view></router-view>
 </template>
 
-<style lang="less" scoped>
-
-</style>
+<style lang="less" scoped></style>
