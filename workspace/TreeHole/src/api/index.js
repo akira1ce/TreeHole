4eff054be3f1f6ef9a9cd5f5bb583fec7c732a70
amos <amoslier@163.com>
2022-11-18 18:06:12 +0800

    [add] add api model

--- workspace/TreeHole/src/api/index.js
+++ workspace/TreeHole/src/api/index.js
@@ -7,4 +7,32 @@ export default {
     modifyById: "/user/modifyById",
     getUserById: "/user/getUserById",
   },
+  tree: {
+    getTreeList: "/tree/getTreeList",
+    getTreeById: "/tree/getTreeById",
+    addTree: "/tree/addTree",
+    removeById: "/tree/removeById",
+    modifyById: "/tree/modifyById",
+  },
+  uploadCenter: {
+    upload: "/uploadCenter/upload",
+    files: "/uploadCenter/files",
+  },
+  record: {
+    getRecordByUserID: "/record/getRecordByUserID",
+    modifyRecord: "/record/modifyRecord",
+  },
+  Order: {
+    getOrderList: "/order/getOrderList",
+    addOrder: "/order/addOrder",
+    removeById: "/order/removeById",
+    modifyById: "/order/modifyById",
+  },
+  Socket: {
+    addSocket: "/socket/addSocket",
+    removeById: "/socket/removeById",
+    modifyById: "/socket/modifyById",
+    getSocketByUserID: "/socket/getSocketByUserID",
+
+  },
 };
