55d6421c3f3f2997ab2ba8f5cf100d9e255a5f09
amos <amoslier@163.com>
2022-11-18 10:59:44 +0800

    [modify] fixed unless code

--- workspace/TreeHole/src/App.vue
+++ workspace/TreeHole/src/App.vue
@@ -2,15 +2,17 @@
 import Sidebar from "./components/Sidebar.vue";
 import Topbar from "./components/Topbar.vue";
 import { useRoute } from "vue-router";
-import { computed, reactive, ref, watch } from "vue-demi";
 
 const route = useRoute();
-const whitelist = ['Login','Register']
+
+// static state
+const whitelist = ["Login", "Register"];
+
 </script>
 
 <template>
-  <Sidebar v-show="whitelist.indexOf(route.name)==-1" />
-  <Topbar v-show="whitelist.indexOf(route.name)==-1" />
+  <Sidebar v-show="whitelist.indexOf(route.name) == -1" />
+  <Topbar v-show="whitelist.indexOf(route.name) == -1" />
   <router-view></router-view>
 </template>
 
