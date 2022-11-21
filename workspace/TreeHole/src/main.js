47c4cc3b79baeafdc7f78f5231b39080261cbe0d
amos <amoslier@163.com>
2022-11-21 20:43:04 +0800

    [add] vue3-photo-preview

--- workspace/TreeHole/src/main.js
+++ workspace/TreeHole/src/main.js
@@ -5,8 +5,12 @@ import router from "./router";
 import "./style.css";
 import 'element-plus/dist/index.css'
 
+import vue3PhotoPreview from 'vue3-photo-preview';
+import 'vue3-photo-preview/dist/index.css';
+
 const app = createApp(App);
 
 app.use(router);
+app.use(vue3PhotoPreview);
 
 router.isReady().then(() => app.mount('#app'))
