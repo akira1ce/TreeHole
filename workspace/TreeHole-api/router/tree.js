386dc0dd37dc77af6bd5dee609c6b5e54441557a
amos <amoslier@163.com>
2022-11-24 09:59:31 +0800

    [modify] multipartyMid -> parent router

--- workspace/TreeHole-api/router/tree.js
+++ workspace/TreeHole-api/router/tree.js
@@ -1,20 +1,18 @@
 const express = require("express");
-const multipart = require("connect-multiparty");
 const treeCtrl = require("../controller/tree");
 
-const multipartyMid = multipart();
 const router = express.Router();
 
 // 添加
-router.post("/addTree", multipartyMid, treeCtrl.addTree);
+router.post("/addTree", treeCtrl.addTree);
 // 删除
-router.post("/removeById", multipartyMid, treeCtrl.removeById);
+router.post("/removeById", treeCtrl.removeById);
 // 修改
-router.post("/modifyById", multipartyMid, treeCtrl.modifyById);
+router.post("/modifyById", treeCtrl.modifyById);
 // 获取树列表
-router.get("/getTreeList", multipartyMid, treeCtrl.getTreeList);
+router.get("/getTreeList", treeCtrl.getTreeList);
 // 获取树
-router.post("/getTreeById", multipartyMid, treeCtrl.getTreeById);
+router.post("/getTreeById", treeCtrl.getTreeById);
 // 获取所有者的树列表
-router.post("/getTreeListByUserID", multipartyMid, treeCtrl.getTreeListByUserID);
+router.post("/getTreeListByUserID", treeCtrl.getTreeListByUserID);
 module.exports = router;
