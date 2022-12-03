edbb095b870ad184a9a2940a5fda941517238a36
amos <amoslier@163.com>
2022-12-03 19:20:37 +0800

    [modify] add beforeEach

--- workspace/TreeHole/src/router/index.js
+++ workspace/TreeHole/src/router/index.js
@@ -1,9 +1,12 @@
 import routes from "./route";
 import { createRouter, createWebHashHistory } from "vue-router";
+import { local } from "../util";
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
 });
-
+router.beforeEach((to, from) => {
+  if (!local.getItem("token") && to.name != "Login") return "/login";
+});
 export default router;
