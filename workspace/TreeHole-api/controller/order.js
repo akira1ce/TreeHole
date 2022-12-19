f93a247679d3dcacecaa3d4b7c08150f6e289d31
amos <amoslier@163.com>
2022-12-19 17:48:32 +0800

    [modify] add modifyByTreeID & companionOrder -> push and splice

--- workspace/TreeHole-api/controller/order.js
+++ workspace/TreeHole-api/controller/order.js
@@ -7,8 +7,14 @@ const companionOrder = async (data) => {
   const userID1 = data.buyerID;
   const userID2 = data.sellerID;
   const records = await Record.find({ userID: { $in: [userID1, userID2] } });
-  records[0].order.push(data._id);
-  records[1].order.push(data._id);
+  const index = records[0].order.indexOf(data._id);
+  if (index == -1) {
+    records[0].order.push(data._id.toString());
+    records[1].order.push(data._id.toString());
+  } else {
+    records[0].order.splice(index, 1);
+    records[1].order.splice(index, 1);
+  }
   await Record.findByIdAndUpdate(records[0]._id, records[0]);
   await Record.findByIdAndUpdate(records[1]._id, records[1]);
 };
@@ -61,6 +67,7 @@ const removeById = async (req, res, next) => {
   try {
     const { _id } = req.body;
     let data = await Order.findByIdAndRemove(_id);
+    await companionOrder(data);
     if (!data) {
       next(err("The order does not exist", 403, ""));
       return;
@@ -85,10 +92,31 @@ const modifyById = async (req, res, next) => {
     next(err(e));
   }
 };
+
+// modifyByTreeID
+const modifyByTreeID = async (req, res, next) => {
+  try {
+    const { treeID, state } = req.body;
+    const order = await Order.findOne({ treeID });
+    if (!order) {
+      next(err("The order does not exist", 403, ""));
+      return;
+    }
+    const data = await Order.findByIdAndUpdate(order._id, { state });
+    if (!data) {
+      next(err("update order error", 403, ""));
+      return;
+    }
+    res.send(result(200, data, "ok"));
+  } catch (e) {
+    next(err(e));
+  }
+};
 module.exports = {
   getOrderList,
   getOrderListByUserID,
   addOrder,
   removeById,
   modifyById,
+  modifyByTreeID,
 };
