1d96b9215067b12df3606622ffd4b6da71f3c178
amos <amoslier@163.com>
2023-03-03 17:51:30 +0800

    【修改】打包文件路径改为相对路径

--- workspace/TreeHole/vite.config.js
+++ workspace/TreeHole/vite.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: Akira
  * @Date: 2022-11-14 09:08:28
- * @LastEditTime: 2023-02-20 16:16:14
+ * @LastEditTime: 2023-03-02 17:05:32
  */
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
@@ -10,6 +10,7 @@ import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 export default defineConfig({
+  base: "./",
   plugins: [
     vue(),
     AutoImport({
