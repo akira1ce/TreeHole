c4968b8946487b6809ce47627f6473317c8535f1
amos <amoslier@163.com>
2023-04-02 16:55:45 +0800

    【修改】添加局部刷新

--- workspace/TreeHole/src/App.vue
+++ workspace/TreeHole/src/App.vue
@@ -1,9 +1,10 @@
 <!--
  * @Author: Akira
  * @Date: 2022-11-14 09:08:28
- * @LastEditTime: 2023-04-01 17:13:17
+ * @LastEditTime: 2023-04-02 14:58:16
 -->
 <script setup>
+import { nextTick, ref } from "vue";
 import Sidebar from "./components/Sidebar.vue";
 import Topbar from "./components/Topbar.vue";
 import { useRoute } from "vue-router";
@@ -11,6 +12,14 @@ import { useRoute } from "vue-router";
 const route = useRoute();
 
 const whitelist = ["Login", "Register"];
+const isRouterAlive = ref(true);
+
+const reload = () => {
+  isRouterAlive.value = false;
+  nextTick(() => {
+    isRouterAlive.value = true;
+  });
+};
 </script>
 
 <template>
@@ -18,11 +27,33 @@ const whitelist = ["Login", "Register"];
   <Sidebar v-if="whitelist.indexOf(route.name) == -1" />
   <!-- 顶部栏 -->
   <Topbar v-if="whitelist.indexOf(route.name) == -1" />
-  <router-view v-slot="{ Component }">
-    <keep-alive :include="['Dynamic', 'Home', 'Space', 'Personal']">
+  <i class="iconfont icon-shuaxin reload" @click="reload" v-if="whitelist.indexOf(route.name) == -1"></i>
+  <router-view v-slot="{ Component }" v-if="isRouterAlive">
+    <keep-alive :include="['Dynamic', 'Home']">
       <component :is="Component" />
     </keep-alive>
   </router-view>
 </template>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.reload {
+  font-weight: bold;
+  font-size: 18px;
+  background-color: rgb(255, 255, 255);
+  padding: 12px;
+  border: 1px solid rgb(227, 229, 231);
+  border-radius: 12px;
+  position: fixed;
+  right: 30px;
+  bottom: 80px;
+  z-index: 1000;
+  cursor: pointer;
+  transition: all 0.1s;
+  &:hover {
+    background-color: rgb(227, 229, 231);
+  }
+  &:active {
+    transform: scale(0.9);
+  }
+}
+</style>
